c0d4c073ba6e39e3df4935c70d3f33ffc31356b8
Gilo-Shalimar <111472836+Gilo-Shalimar@users.noreply.github.com>
2022-08-18 11:09:11 +0800

    Add files via upload

---
+++ Assignment 1.2.java
@@ -0,0 +1,54 @@
+public class App {
+    public static void main(String[] args) {
+
+        //Variable
+        String alaga_ko = "Mingming" ;
+        String codeusername = "klu" ;
+
+        // int - stores integers (whole numbers), without decimals, such as 123 or -123
+        int myAge = 18;
+        int temp = 5;
+
+        // float - stores floating point numbers, with decimals, such as 19.99 or -19.99
+        float myGradeInClass = 4.00f;
+
+        // char - stores single characters, such as 'a' or 'B'. Char values are
+        // surrounded by single quotes
+        char myMiddleInitial = 'D';
+        char myFavoriteNumber = '8' ;
+        char MyFavoriteFictionalchara = '*' ;
+
+        // boolean - stores values with two states: true or false
+        boolean iLoveMangoes = true;
+        boolean iAm7FeetTall = true;
+
+        boolean Freshmankaba = true;
+        boolean highschoolpabaako = false;
+        boolean babypabaako = false;
+
+        // String - stores text, such as "Hello". String values are surrounded by double
+        // quotes
+        String myFirstName = "Shalimar";
+
+        // If you don't want others (or yourself) to overwrite existing values, use the
+        // final keyword (this will declare the variable as "final" or "constant", which
+        // means unchangeable and read-only)
+        final String myLastName = "Gilo";
+
+        System.out.println(myAge);
+        System.out.println(alaga_ko);
+        System.out.println(codeusername);
+        System.out.println(temp);
+        System.out.println(myGradeInClass);
+        System.out.println(myMiddleInitial);
+        System.out.println(myFavoriteNumber);
+        System.out.println(MyFavoriteFictionalchara);
+        System.out.println(iLoveMangoes);
+        System.out.println(iAm7FeetTall);
+        System.out.println(Freshmankaba);
+        System.out.println(highschoolpabaako);
+        System.out.println(babypabaako);
+        System.out.println(myFirstName);
+        System.out.println(myLastName);
+    }
+}
